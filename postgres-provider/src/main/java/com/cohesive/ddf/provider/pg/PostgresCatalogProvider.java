e53c47ba95930806264bbd3f088034edd1575c70
Jeff Vettraino <jeff.vettraino@cohesiveintegrations.com>
2015-05-26 21:32:33 -0700

    Updated the maven bundle version and added in additional logging

--- postgres-provider/src/main/java/com/cohesive/ddf/provider/pg/PostgresCatalogProvider.java
+++ postgres-provider/src/main/java/com/cohesive/ddf/provider/pg/PostgresCatalogProvider.java
@@ -54,7 +54,7 @@ import ddf.security.encryption.EncryptionService;
 
 public class PostgresCatalogProvider extends MaskableImpl implements CatalogProvider {
 
-    private static final Logger logger = LoggerFactory.getLogger( PostgresCatalogProvider.class );
+    private static final Logger LOGGER = LoggerFactory.getLogger( PostgresCatalogProvider.class );
 
     private SourceMonitor callback;
 
@@ -78,6 +78,12 @@ public class PostgresCatalogProvider extends MaskableImpl implements CatalogProv
         this.setOrganization( "Cohesive Integrations" );
         this.setTitle( "PostgreSQL Catalog Provider" );
     }
+    
+    public void destroy(){
+        if ( datasource != null ){
+            datasource.close();
+        }
+    }
 
     public void setDataSource( BoneCPDataSource datasource ) {
         this.datasource = datasource;
@@ -98,7 +104,7 @@ public class PostgresCatalogProvider extends MaskableImpl implements CatalogProv
     @Override
     public boolean isAvailable() {
         try {
-            logger.debug( "Testing if database connection is available (called from CatalogFramework)" );
+            LOGGER.debug( "Testing if database connection is available (called from CatalogFramework)" );
             boolean available = queryMapper.testConnection() == 1;
             if ( callback != null ) {
                 if ( available ) {
@@ -109,8 +115,8 @@ public class PostgresCatalogProvider extends MaskableImpl implements CatalogProv
             }
             return available;
         } catch ( Exception e ) {
-            logger.warn( "Postgres Connection not available: " + e.getMessage() );
-            logger.debug( e.getMessage(), e );
+            LOGGER.warn( "Postgres Connection not available: " + e.getMessage() );
+            LOGGER.debug( e.getMessage(), e );
             return false;
         }
     }
@@ -122,44 +128,47 @@ public class PostgresCatalogProvider extends MaskableImpl implements CatalogProv
     }
 
     public void maskId( String sourceId ) {
-        logger.info( this.getTitle() + " Source ID changed from [" + getId() + "] to [" + sourceId + "]" );
+        LOGGER.info( this.getTitle() + " Source ID changed from [" + getId() + "] to [" + sourceId + "]" );
         super.maskId( sourceId );
     }
 
     @Override
-    @Transactional
     public CreateResponse create( CreateRequest createRequest ) throws IngestException {
         try {
+            
             List<Metacard> createMetacards = createRequest.getMetacards();
             List<Metacard> output = new ArrayList<Metacard>();
-
+            LOGGER.debug( "Attempting to store metadata for {} metacards", createMetacards.size() );
             Date now = new Date();
             String sourceId = getId();
-
             for ( Metacard metacard : createMetacards ) {
                 ProviderMetacard providerMetacard = new ProviderMetacard( metacard, now, sourceId );
                 crudMapper.createMetacard( providerMetacard );
                 output.add( providerMetacard );
+                LOGGER.trace( "Saved Metacard in Postgres Catalog: {}", providerMetacard );
             }
 
             return new CreateResponseImpl( createRequest, null, output );
         } catch ( Exception e ) {
+            LOGGER.warn( "Could not ingest Metacard: {}", e.getMessage(), e );
             throw new IngestException( e.getMessage() );
         } catch ( Throwable t ) {
+            LOGGER.warn( "Could not ingest Metacard: {}", t.getMessage(), t );
             throw new IngestException( t.getMessage() );
         }
     }
 
     @Override
-    @Transactional
     public UpdateResponse update( UpdateRequest updateRequest ) throws IngestException {
         String updateAttribute = updateRequest.getAttributeName();
+        LOGGER.debug( "Attempting to update metadata using the '{}' attribute", updateAttribute );
         List<Update> updates = null;
         if ( UpdateRequest.UPDATE_BY_ID.equals( updateAttribute ) ) {
             updates = updateById( updateRequest );
         } else if ( UpdateRequest.UPDATE_BY_PRODUCT_URI.equals( updateAttribute ) ) {
             updates = updateByResourceURI( updateRequest );
         } else {
+            LOGGER.warn( "Could not update Metacard because the Update By attribute is unknown{}", updateAttribute );
             throw new IngestException( "Updating by '" + updateAttribute + "' is not supported by the " + getTitle() + ", only updating by '" + UpdateRequest.UPDATE_BY_ID + "' or '"
                     + UpdateRequest.UPDATE_BY_PRODUCT_URI + " are supported." );
         }
@@ -168,34 +177,43 @@ public class PostgresCatalogProvider extends MaskableImpl implements CatalogProv
 
     @SuppressWarnings( "unchecked" )
     @Override
-    @Transactional
     public DeleteResponse delete( DeleteRequest deleteRequest ) throws IngestException {
         String deleteAttribute = deleteRequest.getAttributeName();
+        LOGGER.debug( "Attempting to update metadata using the '{}' attribute", deleteAttribute );
         int size = 0;
         List<Metacard> returnCards = null;
         if ( DeleteRequest.DELETE_BY_ID.equals( deleteAttribute ) ) {
             List<String> ids = (List<String>) deleteRequest.getAttributeValues();
             List<Result> results = queryMapper.queryById( ids );
             size = crudMapper.deleteMetacards( ids );
+            LOGGER.trace( "Deleted {} Metacards from Postgres Catalog", size );
             returnCards = new ArrayList<Metacard>( size );
             final String id = getId();
             for ( Result result : results ) {
                 Metacard metacard = result.getMetacard();
                 metacard.setSourceId( id );
                 returnCards.add( metacard );
+                if ( LOGGER.isTraceEnabled() ){
+                    LOGGER.trace( "Deleted Metacard from Postgres Catalog: {}", metacard );
+                }
             }
         } else if ( DeleteRequest.DELETE_BY_PRODUCT_URI.equals( deleteAttribute ) ) {
             List<URI> uris = (List<URI>) deleteRequest.getAttributeValues();
             List<Result> results = queryMapper.queryByURI( uris );
             size = crudMapper.deleteMetacardsByURI( uris );
+            LOGGER.trace( "Deleted {} Metacards from Postgres Catalog", size );
             returnCards = new ArrayList<Metacard>( size );
             final String id = getId();
             for ( Result result : results ) {
                 Metacard metacard = result.getMetacard();
                 metacard.setSourceId( id );
                 returnCards.add( metacard );
+                if ( LOGGER.isTraceEnabled() ){
+                    LOGGER.trace( "Deleted Metacard from Postgres Catalog: {}", metacard );
+                }
             }
         } else {
+            LOGGER.warn( "Could not delete Metacard because the Delete By attribute is unknown{}", deleteAttribute );
             throw new IngestException( "Deleting by '" + deleteAttribute + "' is not supported by the " + getTitle() + ", only deleting by '" + DeleteRequest.DELETE_BY_ID + "' or '"
                     + DeleteRequest.DELETE_BY_PRODUCT_URI + " are supported." );
         }
@@ -207,6 +225,7 @@ public class PostgresCatalogProvider extends MaskableImpl implements CatalogProv
     public SourceResponse query( QueryRequest request ) throws UnsupportedQueryException {
         try {
             Query query = request.getQuery();
+            LOGGER.debug( "Attempting to execute query against Postgres Catalog: {}", query );
 
             SQLHolder holder = filterAdapter.adapt( request.getQuery(), new PostgresFilterDelegate( namespaceHandler ) );
             CatalogQuery providerQuery = new PostgresQuery( query, holder, getId() );
@@ -216,6 +235,7 @@ public class PostgresCatalogProvider extends MaskableImpl implements CatalogProv
             if ( query.requestsTotalResultsCount() ) {
                 int queryPageSize = query.getPageSize();
                 int resultsSize = results.size();
+                LOGGER.debug( "Query returned {} matching results", resultsSize );
                 if ( queryPageSize == resultsSize ) {
                     grandTotal = Long.valueOf( queryMapper.getTotalCount( providerQuery ) );
                 } else {
@@ -228,7 +248,7 @@ public class PostgresCatalogProvider extends MaskableImpl implements CatalogProv
             }
             return new SourceResponseImpl( request, results, grandTotal );
         } catch ( Exception e ) {
-            logger.debug( "Could not execte query: " + e.getMessage(), e );
+            LOGGER.debug( "Could not execte query: " + e.getMessage(), e );
             throw new UnsupportedQueryException( e );
         }
     }
@@ -238,9 +258,10 @@ public class PostgresCatalogProvider extends MaskableImpl implements CatalogProv
         Set<ContentType> types = null;
         try {
             types = new LinkedHashSet<ContentType>( catalogTypesMapper.getContentTypes() );
+            LOGGER.debug( "The Content Types returned from Postgres Metadata Catalog are: {}", types );
         } catch ( Exception e ) {
-            logger.warn( "Could not get Content Types: " + e.getMessage() );
-            logger.debug( e.getMessage(), e );
+            LOGGER.warn( "Could not get Content Types: " + e.getMessage() );
+            LOGGER.debug( e.getMessage(), e );
             types = new HashSet<ContentType>();
         }
         return types;
@@ -255,7 +276,7 @@ public class PostgresCatalogProvider extends MaskableImpl implements CatalogProv
             // System.out.println( "Resource URI (Update): " + metacard.getResourceURI() );
             // System.out.println( "Resource Size (Update): " + metacard.getResourceSize() );
             int size = crudMapper.updateMetacardByURI( new UpdateByURIRequest( metacard, ((URI) entry.getKey()).toString() ) );
-
+            LOGGER.debug( "Updated {} metacards by ResourceID", size );
             returnUpdates.add( new UpdateImpl( metacard, null ) );
         }
         return returnUpdates;
@@ -270,94 +291,95 @@ public class PostgresCatalogProvider extends MaskableImpl implements CatalogProv
             // System.out.println( "Resource URI (Update): " + metacard.getResourceURI() );
             // System.out.println( "Resource Size (Update): " + metacard.getResourceSize() );
             int size = crudMapper.updateMetacard( metacard );
+            LOGGER.debug( "Updated {} metacards by Catalog ID", size );
             returnUpdates.add( new UpdateImpl( metacard, null ) );
         }
         return returnUpdates;
     }
 
     public void updateDataSource( @SuppressWarnings( "rawtypes" ) Map properties ) {
-
+        LOGGER.debug( "Attempting to update the Postgres Datasource with values: {}", properties );
         if ( datasource == null ) {
-            logger.warn( "DataSource is null and has not been properly initialized. This object should be created before it is updated." );
+            LOGGER.warn( "DataSource is null and has not been properly initialized. This object should be created before it is updated." );
             return;
         }
         Object propertyObject = properties.get( "url" );
         if ( propertyObject != null ) {
             datasource.setJdbcUrl( propertyObject.toString() );
-            logger.info( "Set Database URL to [" + propertyObject + "]" );
+            LOGGER.info( "Set Database URL to [" + propertyObject + "]" );
         }
 
         propertyObject = properties.get( "username" );
         if ( propertyObject != null ) {
             datasource.setUsername( propertyObject.toString() );
-            logger.info( "Set Database Username to [" + propertyObject + "]" );
+            LOGGER.info( "Set Database Username to [" + propertyObject + "]" );
         }
 
         propertyObject = properties.get( "password" );
         if ( propertyObject != null ) {
             String decryptedPassword = decrypt( propertyObject.toString() );
             datasource.setPassword( decryptedPassword );
-            logger.info( "Set Database Password." );
+            LOGGER.info( "Set Database Password." );
         }
 
         propertyObject = properties.get( "idleConnectionTestPeriodInMinutes" );
         try {
             if ( propertyObject != null ) {
                 datasource.setIdleConnectionTestPeriodInMinutes( Long.valueOf( propertyObject.toString() ) );
-                logger.info( "Set idleConnectionTestPeriodInMinutes to [" + propertyObject + "]" );
+                LOGGER.info( "Set idleConnectionTestPeriodInMinutes to [" + propertyObject + "]" );
             }
         } catch ( NumberFormatException e ) {
-            logger.info( "This property {idleConnectionTestPeriodInMinutes} requires that the input be an integer value. ", e );
+            LOGGER.info( "This property {idleConnectionTestPeriodInMinutes} requires that the input be an integer value. ", e );
         }
 
         propertyObject = properties.get( "idleMaxAgeInMinutes" );
         try {
             if ( propertyObject != null ) {
                 datasource.setIdleMaxAgeInMinutes( Long.valueOf( propertyObject.toString() ) );
-                logger.info( "Set idleMaxAgeInMinutes to [" + propertyObject + "]" );
+                LOGGER.info( "Set idleMaxAgeInMinutes to [" + propertyObject + "]" );
             }
         } catch ( NumberFormatException e ) {
-            logger.info( "This property {idleMaxAgeInMinutes} requires that the input be an integer value. ", e );
+            LOGGER.info( "This property {idleMaxAgeInMinutes} requires that the input be an integer value. ", e );
         }
 
         propertyObject = properties.get( "maxConnectionsPerPartition" );
         try {
             if ( propertyObject != null ) {
                 datasource.setMaxConnectionsPerPartition( Integer.valueOf( propertyObject.toString() ) );
-                logger.info( "Set maxConnectionsPerPartition to [" + propertyObject + "]" );
+                LOGGER.info( "Set maxConnectionsPerPartition to [" + propertyObject + "]" );
             }
         } catch ( NumberFormatException e ) {
-            logger.info( "This property {maxConnectionsPerPartition} requires that the input be an integer value. ", e );
+            LOGGER.info( "This property {maxConnectionsPerPartition} requires that the input be an integer value. ", e );
         }
 
         propertyObject = properties.get( "partitionCount" );
         try {
             if ( propertyObject != null ) {
                 datasource.setPartitionCount( Integer.valueOf( propertyObject.toString() ) );
-                logger.info( "Set partitionCount to [" + propertyObject + "]" );
+                LOGGER.info( "Set partitionCount to [" + propertyObject + "]" );
             }
         } catch ( NumberFormatException e ) {
-            logger.info( "This property {partitionCount} requires that the input be an integer value. ", e );
+            LOGGER.info( "This property {partitionCount} requires that the input be an integer value. ", e );
         }
 
         propertyObject = properties.get( "acquireIncrement" );
         try {
             if ( propertyObject != null ) {
                 datasource.setAcquireIncrement( Integer.valueOf( propertyObject.toString() ) );
-                logger.info( "Set acquireIncrement to [" + propertyObject + "]" );
+                LOGGER.info( "Set acquireIncrement to [" + propertyObject + "]" );
             }
         } catch ( NumberFormatException e ) {
-            logger.info( "This property {acquireIncrement} requires that the input be an integer value. ", e );
+            LOGGER.info( "This property {acquireIncrement} requires that the input be an integer value. ", e );
         }
 
         propertyObject = properties.get( "statementsCacheSize" );
         try {
             if ( propertyObject != null ) {
                 datasource.setStatementsCacheSize( Integer.valueOf( propertyObject.toString() ) );
-                logger.info( "Set statementsCacheSize to [" + propertyObject + "]" );
+                LOGGER.info( "Set statementsCacheSize to [" + propertyObject + "]" );
             }
         } catch ( NumberFormatException e ) {
-            logger.info( "This property {statementsCacheSize} requires that the input be an integer value. ", e );
+            LOGGER.info( "This property {statementsCacheSize} requires that the input be an integer value. ", e );
         }
 
         if ( callback != null ) {
@@ -367,7 +389,7 @@ public class PostgresCatalogProvider extends MaskableImpl implements CatalogProv
                 callback.setUnavailable();
             }
         } else {
-            logger.debug( "Availability Callback is null." );
+            LOGGER.debug( "Availability Callback is null." );
         }
     }
 
@@ -394,12 +416,12 @@ public class PostgresCatalogProvider extends MaskableImpl implements CatalogProv
         String encryptedValue = unwrapEncryptedValue( wrappedEncryptedValue );
 
         if ( wrappedEncryptedValue == null || wrappedEncryptedValue.isEmpty() ) {
-            logger.error( "A blank password was provided in the Postgres Catalog Provider configuration in the console." );
+            LOGGER.error( "A blank password was provided in the Postgres Catalog Provider configuration in the console." );
             decryptedValue = null;
         } else if ( encryptionService != null && !wrappedEncryptedValue.equals( encryptedValue ) ) {
             decryptedValue = encryptionService.decrypt( encryptedValue );
         } else if ( encryptionService == null && !wrappedEncryptedValue.equals( encryptedValue ) ) {
-            logger.error( "The Postgres Catalog Provider has a null Encryption Service.  Unable to attempt to decrypt the encrypted Postgres password: " + encryptedValue
+            LOGGER.error( "The Postgres Catalog Provider has a null Encryption Service.  Unable to attempt to decrypt the encrypted Postgres password: " + encryptedValue
                     + ".  Setting decrypted password to null." );
             decryptedValue = null;
         } else if ( wrappedEncryptedValue.equals( encryptedValue ) ) {
@@ -407,7 +429,7 @@ public class PostgresCatalogProvider extends MaskableImpl implements CatalogProv
              * If the password is not in the form ENC(my-encrypted-password), we assume the password is not encrypted.
              */
             decryptedValue = wrappedEncryptedValue;
-            logger.warn( "A plain text password was provided in the Postgres Catalog Provider configuration in the console.  Consider using an encrypted password." );
+            LOGGER.warn( "A plain text password was provided in the Postgres Catalog Provider configuration in the console.  Consider using an encrypted password." );
         }
 
         /*
@@ -442,7 +464,7 @@ public class PostgresCatalogProvider extends MaskableImpl implements CatalogProv
              * If the password is not in the form ENC(my-encrypted-password), we assume the password is not encrypted.
              */
             unwrappedEncryptedValue = wrappedEncryptedValue;
-            logger.warn( "You have provided a plain text password in your " + "Postgres Provider configuration.  Consider using an encrypted password." );
+            LOGGER.warn( "You have provided a plain text password in your " + "Postgres Provider configuration.  Consider using an encrypted password." );
         }
 
         return unwrappedEncryptedValue;
